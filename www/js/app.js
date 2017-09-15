4cb90f15f5f13659aacbf90856a22a9fc623f874
Austin <ajwgeek@gmail.com>
2017-09-14 22:34:23 -0400

    Allowed screen to adjust when keyboard is displayed

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
 		if (window.cordova && window.cordova.plugins.Keyboard) {
-		  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true); // Hide the accessory bar by default.
-		  cordova.plugins.Keyboard.disableScroll(true);
+		  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		  cordova.plugins.Keyboard.disableScroll(false);
 		}
 		if (window.StatusBar) {
 		  StatusBar.styleDefault(); // Require Cordova status bar.
