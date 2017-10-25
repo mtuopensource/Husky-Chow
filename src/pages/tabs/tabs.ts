cc5d7dbdd9c1f2ec783abcc998379175c94425d1
Austin Walhof <ajwgeek@gmail.com>
2017-10-24 22:16:42 -0400

    Added brief documentation to tab controller

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { DailyPage } from '../daily/daily';
 import { TomorrowPage } from '../tomorrow/tomorrow';
 import { CalendarPage } from '../calendar/calendar';
@@ -7,12 +6,20 @@ import { CalendarPage } from '../calendar/calendar';
 @Component({
   templateUrl: 'tabs.html'
 })
+
+/**
+ * TabsPage
+ * The view controller for the tabs shown at the bottom of menu pages. Includes
+ * daily, tomorrow, and calendar pages.
+ */
 export class TabsPage {
+  private tab1Root = DailyPage;
+  private tab2Root = TomorrowPage;
+  private tab3Root = CalendarPage;
 
-  tab1Root = DailyPage;
-  tab2Root = TomorrowPage;
-  tab3Root = CalendarPage;
+  /**
+   * TabsPage Constructor
+   */
   constructor() {
-
   }
 }
